c5b96128e96487f54beb384d9f0be58e9c997547
akhilles <akhilvelagapudi@gmail.com>
2025-10-30 19:22:11 -0400

    Don't add unnecessary __<>__ physical value properties

--- crates/pcb-zen-core/src/lang/component.rs
+++ crates/pcb-zen-core/src/lang/component.rs
@@ -911,12 +911,6 @@ where
                             .unpack_str()
                             .map(|s| s.to_owned())
                             .unwrap_or_else(|| k_val.to_string());
-                        try_add_physical_property(
-                            eval_ctx.heap(),
-                            &mut properties_map,
-                            &key_str,
-                            &v_val,
-                        );
                         properties_map.insert(key_str, v_val);
                     }
                 } else {
@@ -1072,21 +1066,6 @@ where
     fn eval_type(&self) -> Option<starlark::typing::Ty> {
         Some(<ComponentType as StarlarkValue>::get_type_starlark_repr())
     }
-}
-
-fn try_add_physical_property<'a, 'b>(
-    heap: &'a Heap,
-    map: &mut SmallMap<String, Value<'a>>,
-    key: &str,
-    value: &Value<'b>,
-) -> Option<PhysicalValue> {
-    if let Some(val) = value.unpack_str() {
-        if let Ok(physical) = val.parse::<PhysicalValue>() {
-            let key = format!("__{}__", key.to_ascii_lowercase());
-            map.insert(key, heap.alloc(physical));
-        }
-    }
-    None
 }
 
 impl std::fmt::Display for ComponentType {
